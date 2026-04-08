db920da17a2c0f000060d092fcf039b30e777f91
Aktiar H <aktiardocuments@gmail.com>
2026-04-08 12:46:56 +0530

    km';l;'

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Optional: create this file if you want Tailwind styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
